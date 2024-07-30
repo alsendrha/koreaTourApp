2e6cae6489b6ed42c448d6becab457b2f86d77b0
Minyoung <alsendrha@naver.com>
2024-07-30 19:00:09 +0900

    구글맵 안나오는 문제 해결 위도 경도 서로 바껴서 안나오는거였음

--- src/components/detailItem/Map.tsx
+++ src/components/detailItem/Map.tsx
@@ -7,38 +7,30 @@ type MapProps = {
 };
 
 const Map = ({ itemX, itemY }: MapProps) => {
-  console.log(itemX);
-  console.log(itemY);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
   });
 
-  const lat = Number(itemX);
-  const lng = Number(itemY);
-
-  const center = { lat: lat, lng: lng };
   return (
     <div>
       {itemX && (
-        <div className="w-[500px] h-[600px]">
+        <div className="w-[1000px] h-[500px]">
           {isLoaded && (
             <GoogleMap
-              center={center}
+              center={{ lat: Number(itemY), lng: Number(itemX) }}
               zoom={17}
               mapContainerStyle={{ width: "100%", height: "100%" }}
               options={{
                 zoomControl: false,
                 scrollwheel: true,
               }}
-            >
-              {/* <Marker
-                  icon={{
-                    url: "/images/logo1.png",
-                    scaledSize: new window.google.maps.Size(40, 55),
-                  }}
-                  position={{ lat: Number(item.mapx), lng: Number(item.mapy) }}
-                /> */}
-            </GoogleMap>
+              onLoad={(map) => {
+                new window.google.maps.marker.AdvancedMarkerElement({
+                  position: { lat: Number(itemY), lng: Number(itemX) },
+                  map,
+                });
+              }}
+            ></GoogleMap>
           )}
         </div>
       )}
