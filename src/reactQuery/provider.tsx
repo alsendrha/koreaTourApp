8651f2adceb58b8eff50c94894b63c352c7a951c
Minyoung <alsendrha@naver.com>
2024-08-05 18:30:17 +0900

    개발환경에서만 안된다는걸 알게됨

--- src/reactQuery/provider.tsx
+++ src/reactQuery/provider.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren, useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren } from "react";
 
 const ReactQueryProvider = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000,
-          gcTime: 60 * 1000,
-        },
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        gcTime: 60 * 1000,
       },
-    })
-  );
+    },
+  });
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 };
 
