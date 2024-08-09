6ee7caa7c36356c3bb340a12422e807880933913
Minyoung <alsendrha@naver.com>
2024-08-09 14:58:44 +0900

    콘솔로그 삭제 및 디테일 리스트 파일 이미지 컴포넌트로 바꾸기

--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="w-full h-[90px] bg-gray-400 flex items-center">
+    <div className="w-full h-[90px] bg-gray-400 flex items-center justify-center text-4xl font-bold">
       <div>
         <Link href={"/"}>
           <div>우리나라 관광지</div>
